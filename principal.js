5910d83d685308fa575ecfb941bc135372af3637
Gilmar Silva <gsilvabrow@gmail.com>
2018-09-10 21:48:04 -0300

    Revert "vai com Deus"
    This reverts commit 283924bc9e2a274608607b41e0c09b782dbbf15a.

--- principal.js
+++ principal.js
@@ -1,10 +1,1 @@
-var banners = 
-	["Os melhores do Brasil!","Qualidade e prço baixo"];
-var bannerAtual = 0;
-
-function trocaBanner(){
-	bannerAtual = (bannerAtual + ) % 2;
-	document.querySeletor('h2#mensagem').textContent =
-		banners[bannerAtual];
-}
-setIterval(trocaBanner, 2000);
+var banners =
